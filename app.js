4685d44ffde9a5a35ed971f1f444771da87304d0
TheEMP <gunnarDebate@gmail.com>
2017-01-18 15:39:31 -0700

    Final

--- app.js
+++ app.js
@@ -27,11 +27,13 @@ function play(c) {
             helperheader.innerHTML = "Paper covers Rock!"
             playercard.className = "card green darken-1"
             compcard.className = "card red darken-1"
+             Materialize.toast("YOU WON!", 2000);
             return
         }
         helperheader.innerHTML = "Rock beats " + pcname
         playercard.className = "card red darken-1"
         compcard.className = "card green darken-1"
+          Materialize.toast("YOU LOSE!", 2000);
         return
     }
     else if (compchoice === 1) {
@@ -39,21 +41,25 @@ function play(c) {
             helperheader.innerHTML = "Scissors cuts paper!"
             playercard.className = "card green darken-1"
             compcard.className = "card red darken-1"
+             Materialize.toast("YOU WON!", 2000);
             return
         }
         playercard.className = "card red darken-1"
         compcard.className = "card green darken-1"
         helperheader.innerHTML = "Paper beats " + pcname
+        Materialize.toast("YOU LOSE!", 2000);
     }
     else if (compchoice === 2) {
         if (c === 0) {
             helperheader.innerHTML = "Rock smashes Scissors"
             playercard.className = "card green darken-1"
             compcard.className = "card red darken-1"
+            Materialize.toast("YOU WON!", 2000);
             return
         }
         playercard.className = "card red darken-1"
         compcard.className = "card green darken-1"
          helperheader.innerHTML = "Scissors beats " + pcname
+         Materialize.toast("YOU LOSE!", 2000);
     }
 }
